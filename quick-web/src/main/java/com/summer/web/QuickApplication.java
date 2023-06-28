fab434253bb4543c5b2b4436b46a49e73e69f8ce
chenhao <1183829953@qq.com>
2023-06-28 23:11:08 +0800

    提交初步版本

---
+++ quick-web/src/main/java/com/summer/web/QuickApplication.java
@@ -0,0 +1,17 @@
+package com.summer.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: quickAppliction
+ * @Author: chenhao
+ * @Date: 2023/6/28
+ */
+@SpringBootApplication
+public class QuickApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuickApplication.class,args);
+
+    }
+}
